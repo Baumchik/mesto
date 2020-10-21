202d2ae5a7e3ca556be3cc9d91f2b987215f23b3
Baumchik <73245682+Baumchik@users.noreply.github.com>
2020-10-22 00:23:13 +0800

    test

---
+++ index.php
@@ -0,0 +1,67 @@
+</<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//RU" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="ru">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta name="viewport" content="width=device-width>
+    <title>Поиск по номеру телефона</title>
+    <link rel="stylesheet" href="styles/normalize.css">
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body class="bodys">
+<div class="wrapper"></div>
+
+<div class="header-wrapper">
+
+   <a href="https://hits.pctraffic.net/click?pid=131&offer_id=2"> <img class="header-wrapper__logo" src="images/logo.png"></a>
+
+    <p class="header-wrapper_p">
+        Определить местоположение телефона по номеру
+    </p>
+</div>
+
+<div class="page">
+
+    <a class="page__button" href="https://hits.pctraffic.net/click?pid=131&offer_id=2">ВВЕСТИ НОМЕР</a>
+    <img class="page_image" src="images/general-image.png">
+
+</div>
+<footer class="footer">
+
+    <div class="footer-wrapper">
+        <div class="footer-wrapper__2020">
+        <img class="footer-wrapper__logo" src="images/logo.png">
+        <p> © 2020. Все права защищены </p>
+        </div>
+        <p>TIRASS HOLDING LTD<br>
+            Unit 111383, 2nd Floor, 6<br> Market Place, London, <br>Fitzrovia, United Kingdom,<br> W1W 8AF</p>
+
+        <img src="images/pay-visa.jpg">
+        <img src="images/pay-master.png">
+        <img src="images/pay-mir.png">
+
+        <div class="footer_wrapper__url">
+        <a  href="/terms.html">Условия пользования сайтом</a>
+        <a href="/privacy.html">Политика конфиденциальности</a>
+
+        </div>
+    </div>
+
+
+    <p>
+
+        Точность определения координат зависит от множества факторов. В нормальных условиях она составляет от 1 до 20 метров. В редких случаях – до нескольких километров.
+    </p>
+
+    <p>
+        Определение координат возможно для любых современных устройств, оснащённых датчиками GPS и/или Glonass, GSM модулем и имеющих возможность выхода в Интернет. Под это определение попадает более 95% современных смартфонов и планшетных компьютеров.
+    </p>
+
+    <p>
+        Работа нашего сервиса не зависит от производителя и операционной системы отслеживаемого устройства.
+    </p>
+</footer>
+
+</body>
+
+
+</html>
